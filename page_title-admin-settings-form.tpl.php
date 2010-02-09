9c7e74dc8c07271b8dbec3456b7ef18dcb9895d7
Nicholas Thompson <njt1982@gmail.com>
2010-02-09 13:35:22 +0000

    Some cody clean-up, admin interface tweaks to make it more user friendly and fixes for comment reply pages

--- page_title-admin-settings-form.tpl.php
+++ page_title-admin-settings-form.tpl.php
@@ -17,15 +17,29 @@ foreach (element_children($form['pattern']) as $key) {
   unset($form['pattern'][$key]['#title']);
 
   $row = array(
-    drupal_render($title),
-    drupal_render($form['scope'][$key]),
-    drupal_render($form['pattern'][$key]),
-    isset($form['showfield'][$key .'_showfield']) ? drupal_render($form['showfield'][$key .'_showfield']) : '',
+    array('data' => drupal_render($title), 'class' => 'page-type'),
+    array('data' => drupal_render($form['scope'][$key]), 'class' => 'scope'),
   );
+  if (isset($form['showfield'][$key .'_showfield'])) {
+    $row[] = array('data' => drupal_render($form['pattern'][$key]), 'class' => 'pattern');
+    $row[] = array('data' => drupal_render($form['showfield'][$key .'_showfield']), 'class' => 'showfield');
+  }
+  else {
+    $row[] = array('data' => drupal_render($form['pattern'][$key]), 'colspan' => 2, 'class' => 'pattern');
+  }
 
   $rows[] = $row;
 }
 
-print theme('table', array(t('Page Type'), t('Token Scope'), t('Pattern'), t('Show Field')), $rows);
+$headers = array(
+  array('data' => t('Page Type'), 'class' => 'page-type'),
+  array('data' => t('Token Scope'), 'class' => 'scope'),
+  array('data' => t('Pattern'), 'class' => 'pattern'),
+  array('data' => t('Show Field'), 'class' => 'showfie;d'),
+);
+
+
+drupal_add_css(drupal_get_path('module', 'page_title') .'/page_title.admin.css', 'module', 'all', FALSE);
+print theme('table', $headers, $rows, array('id' => 'page-title-settings'));
 
 print drupal_render($form);
